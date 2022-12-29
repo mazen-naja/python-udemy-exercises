429a05cee97e17f8f1ae3856af507bab7438c216
Mazen Naja <63180234+mazen-naja@users.noreply.github.com>
2022-12-29 17:56:17 +0200

    Adding Ex2

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ def calculate_sum(a,b):
 a=2
 b=3
 print(a,"+",b,"=",calculate_sum(a,b))
+
+
+
+#Excersie 2
+
+def is_square_root(x,y):
+    
+    if (abs(y)==abs(x)**0.5):
+        return True
+    return False
+
+x=4
+y=2
+print (is_square_root(x,y))
